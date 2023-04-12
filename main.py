3c0bcb17f3316239bf4bc29de2b64f84f0810825
Artem Lubentsov <artem.law@mail.ru>
2023-04-12 15:30:17 +0300

    Add task №1 result

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import requests
 
+
+def find_hero_max_intelligence():
+    heroes = ['Hulk', 'Captain America', 'Thanos']
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    resp = requests.get(url=url)
+
+    hero_max_intelligence = max([(hero['name'], hero['powerstats']['intelligence']) for hero in resp.json() if hero['name'] in heroes])[0]
+    return hero_max_intelligence
+
+
+if __name__ == '__main__':
+    print('Cамый умный герой:', find_hero_max_intelligence())
